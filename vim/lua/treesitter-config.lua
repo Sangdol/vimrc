b9b89731531835ac0aec9d31ae339c0cd65b10b3
Sanghyun Lee <hammerha@gmail.com>
2023-03-07 23:18:20 +0100

    Add yaml to treesitter to enable syntax highlight with tundra

--- vim/lua/treesitter-config.lua
+++ vim/lua/treesitter-config.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = {'lua', 'python', 'typescript', 'javascript', 'ruby', 'bash', 'css', 'html', 'json', 'sql', 'vim', 'help'},
+  ensure_installed = {'lua', 'python', 'typescript', 'javascript', 'ruby', 'bash', 'css', 'html', 'json', 'sql', 'vim', 'help', 'yaml'},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
