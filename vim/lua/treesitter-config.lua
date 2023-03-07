3032a0b1bcebd4ef763bd3a4d637089a17cb01ee
Sanghyun Lee <hammerha@gmail.com>
2023-03-07 23:19:16 +0100

    Add a newline

--- vim/lua/treesitter-config.lua
+++ vim/lua/treesitter-config.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = {'lua', 'python', 'typescript', 'javascript', 'ruby', 'bash', 'css', 'html', 'json', 'sql', 'vim', 'help', 'yaml'},
+  ensure_installed = {'lua', 'python', 'typescript', 'javascript', 'ruby',
+    'bash', 'css', 'html', 'json', 'sql', 'vim', 'help', 'yaml'},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
