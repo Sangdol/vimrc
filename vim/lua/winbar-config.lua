3e6914a600142d8e958d173020ff382a12a247ca
Sanghyun Lee <hammerha@gmail.com>
2023-09-09 12:54:33 +0200

    Add a visual cue for an empty winbar

--- vim/lua/winbar-config.lua
+++ vim/lua/winbar-config.lua
@@ -3,7 +3,7 @@ function _G.symbol_line()
   local curwin = vim.g.statusline_winid or 0
   local curbuf = vim.api.nvim_win_get_buf(curwin)
   local ok, line = pcall(vim.api.nvim_buf_get_var, curbuf, 'coc_symbol_line')
-  return ok and line or ''
+  return ok and line or ' ⋮'
 end
 
 vim.o.winbar = '%!v:lua.symbol_line()'
