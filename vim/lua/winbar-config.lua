45df801bfc03702a05fe666612945c5b331baea4
Sanghyun Lee <hammerha@gmail.com>
2024-01-06 20:54:57 +0100

    Add window number to the winbar

--- vim/lua/winbar-config.lua
+++ vim/lua/winbar-config.lua
@@ -3,7 +3,8 @@ function _G.symbol_line()
   local curwin = vim.g.statusline_winid or 0
   local curbuf = vim.api.nvim_win_get_buf(curwin)
   local ok, line = pcall(vim.api.nvim_buf_get_var, curbuf, 'coc_symbol_line')
-  return ok and line or ' ⋮'
+  -- show window number
+  return ok and line or ' ⋮' .. vim.api.nvim_win_get_number(curwin)
 end
 
 vim.o.winbar = '%!v:lua.symbol_line()'
